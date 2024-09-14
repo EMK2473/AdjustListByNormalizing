13e9c718fde18569c425ed2ef79bbb40b15719c0
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 13:16:17 -0400

    declared and assigned variables; looped through inputArray to print out values for testing

--- AdjustListByNormalizing.java
+++ AdjustListByNormalizing.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class AdjustListByNormalizing {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+      int NUM_ELEMENTS = scnr.nextInt();
+      System.out.println(NUM_ELEMENTS);
+
+      double[] inputValues = new double[NUM_ELEMENTS];
+
+      for(int i = 0; i < NUM_ELEMENTS; ++i){
+        inputValues[i] = scnr.nextDouble();
+        System.out.println(inputValues[i]);
+
+      }
+      scnr.close();
    }
 }

23c3246f375a6a9816e9e89f36676b2a0218a230
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 13:26:10 -0400

    removed testing output prints; adjusted output format for submission

--- AdjustListByNormalizing.java
+++ AdjustListByNormalizing.java
@@ -6,13 +6,11 @@ public class AdjustListByNormalizing {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int NUM_ELEMENTS = scnr.nextInt();
-      System.out.println(NUM_ELEMENTS);
 
       double[] inputValues = new double[NUM_ELEMENTS];
 
       for(int i = 0; i < NUM_ELEMENTS; ++i){
         inputValues[i] = scnr.nextDouble();
-        System.out.println(inputValues[i]);
       }
       
       double highestValue = inputValues[0];
@@ -22,15 +20,14 @@ public class AdjustListByNormalizing {
             highestValue = inputValues[i];
         }
       }
-      System.out.println("Highest value: " + highestValue);
 
       for(int i = 0; i < inputValues.length; ++i){
         inputValues[i] = inputValues[i] / highestValue;
-        System.out.println("Values divided by highest value: " + inputValues[i]);
-
+        System.out.printf("%.2f", inputValues[i]);
+        System.out.print(" ");
       }
 
-
+      System.out.println();
       scnr.close();
    }
 }

5dffcb64429086fd269c3d4d690b1b668b7ff07b
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 12:51:41 -0400

    init file

---
+++ AdjustListByNormalizing.java
@@ -0,0 +1,9 @@
+package AdjustListByNormalizing;
+
+import java.util.Scanner; 
+
+public class AdjustListByNormalizing {
+   public static void main(String[] args) {
+      /* Type your code here. */
+   }
+}
